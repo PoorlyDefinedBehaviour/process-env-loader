de173c0c254c231aab2508bb7c58071401947699
Bruno <brunotj2015@hotmail.com>
2019-07-10 02:57:29 -0300

    fixing bug where only typescript version is imported

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+declare const _default: (path?: string | undefined, separator?: string) => void;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const path_1 = require("path");
+function* tokenize(content, separator = "=") {
+    if (!content)
+        return;
+    const tokens = content.split(/\n/);
+    if (!tokens)
+        return;
+    for (const token of tokens) {
+        const [property, value] = token.split(separator);
+        yield [property, value];
+    }
+}
+exports.default = (path, separator = "=") => {
+    const file = fs_1.readFileSync(path_1.join(path || process.cwd(), ".env"), { encoding: "utf8" });
+    const file_tokens = tokenize(file, separator);
+    if (file_tokens) {
+        for (const [property, value] of file_tokens) {
+            process.env[property] = value;
+        }
+    }
+};
+//# sourceMappingURL=index.js.map
