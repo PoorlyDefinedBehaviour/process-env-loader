1ee39dd62eeb71a653bf0a21826a52f8564ffa12
Bruno <brunotj2015@hotmail.com>
2019-07-10 02:14:39 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+type Maybe<T> = T | null | undefined;
+
+function* tokenize(
+  content: string,
+  separator: string = "="
+): Maybe<IterableIterator<Array<string>>> {
+  if (!content) return;
+
+  const tokens: Maybe<Array<string>> = content.split(/\n/);
+
+  if (!tokens) return;
+
+  for (const token of tokens) {
+    const [property, value] = token.split(separator);
+    yield [property, value];
+  }
+}
+
+export default (path?: string, separator: string = "=") => {
+  const file: Maybe<string> = readFileSync(
+    join(path || process.cwd(), ".env"),
+    { encoding: "utf8" }
+  );
+
+  const file_tokens: Maybe<IterableIterator<Array<string>>> = tokenize(
+    file,
+    separator
+  );
+
+  if (file_tokens) {
+    for (const [property, value] of file_tokens) {
+      process.env[property] = value;
+    }
+  }
+};
